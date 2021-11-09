33cc311875a93a54cc8e7ccee142c15b94a7d286
Julian Buchholz <github@julianbuchholz.com>
2021-11-09 14:50:27 +0100

    Add URL Params

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Stuff from './pages/stuff/Stuff';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
+      <h1>This is a website</h1>
       <Routes>
         <Route path="/" element={<p>Hallo</p>} />
         <Route path="/about" element={<p>About</p>} />
         <Route path="/motivation" element={<p>Motivation</p>} />
+        <Route path="/stuff" element={<Stuff />}>
+          <Route path=":stuffParams" element={<Stuff />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
