493cbb71c8a20a9e533f5634cd93044cceb32e62
Patrick Möller <patrick_moeller@live.de>
2021-11-12 16:56:15 +0100

    Adding Routing to form-element

--- src/app/components/AddForm/AddForm.tsx
+++ src/app/components/AddForm/AddForm.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
 import styled from 'styled-components';
 
 export default function AddForm(): JSX.Element {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  const navigate = useNavigate();
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    await fetch('https://json-server.neuefische.de/stuff', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: name, description: description }),
+    });
+    navigate('/');
+  }
+
   return (
     <>
-      <Form action="">
-        <input type="text" />
-        <input type="text" />
+      <Form onSubmit={handleSubmit}>
+        <Label>
+          Name:{' '}
+          <input
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </Label>
+        <Label>
+          Description:{' '}
+          <input
+            type="text"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          />
+        </Label>
         <button type="submit">Click me fool!</button>
       </Form>
     </>
@@ -21,4 +53,8 @@ const Form = styled.form`
   margin: 0.5rem;
   padding: 1rem;
   border-radius: 0.5rem;
+`;
+
+const Label = styled.label`
+  color: green;
 `;
