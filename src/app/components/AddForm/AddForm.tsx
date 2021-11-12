778b21d8d2ea33628ee707ed29e7e1238755b87e
Patrick Möller <patrick_moeller@live.de>
2021-11-12 14:29:27 +0100

    Adding structure to the form

---
+++ src/app/components/AddForm/AddForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function AddForm(): JSX.Element {
+  return (
+    <>
+      <form action="">
+        <input type="text" />
+        <input type="text" />
+        <button type="submit">Click me fool!</button>
+      </form>
+    </>
+  );
+}
