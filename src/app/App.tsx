84588e2c963ea765ccb09c244a9d6a677bca5531
Patrick Möller <patrick_moeller@live.de>
2021-11-12 14:21:33 +0100

    Adding Route directory to page add

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,8 +9,9 @@ function App(): JSX.Element {
       <h1>This is a website</h1>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/about" element={<p>About</p>} />
-        <Route path="/motivation" element={<p>Motivation</p>} />
+        {/* <Route path="/about" element={<p>About</p>} />
+        <Route path="/motivation" element={<p>Motivation</p>} /> */}
+        <Route path="/add" element={<h1>Add Form!</h1>} />
         <Route path="/stuff" element={<Stuff />}>
           <Route path=":thingID" element={<Stuff />}></Route>
         </Route>
